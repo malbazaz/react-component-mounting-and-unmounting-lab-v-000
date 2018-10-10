c3a0a45cf429c167f354071e648a9892bc0b6fe1
Mo Al <malbazaz@mba2018.hbs.edu>
2018-10-10 20:37:37 +0000

    automatically backed up by learn

--- src/components/Pancake.js
+++ src/components/Pancake.js
@@ -16,7 +16,7 @@ componentDidMount(){
 }
   // TODO: create a componentWillUnmount() which will clear the interval
 componentWillUnmount(){
-
+  clearUpInterval()
 }
   updateCounter = () => {
     this.setState({
